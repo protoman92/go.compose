debed95ac2eded97b88d7dfeb495b49e9fd755df
Hai Pham <viethai.pham@holmusk.com>
2018-03-12 11:46:30 +0800

    Added DelayRetry and RetryWithDelay

--- pkg/retry.go
+++ pkg/retry.go
@@ -1,15 +1,25 @@
 package compose
 
-// ErrorFunc represents a function that returns an error.
+import (
+	"time"
+)
+
+// ErrorFunc represents an error-returning function.
 type ErrorFunc func() error
 
-// Retry composes an ErrorFunc with retry capabilities.
-func Retry(f ErrorFunc, retryCount uint) ErrorFunc {
-	return func() error {
-		var retryF func(uint) error
+// RetryIndexErrorFunc represents an error-returning function that also tracks
+// the current retry index.
+type RetryIndexErrorFunc func(uint) error
+
+// CountRetry composes an error function with retry capabilities. The error
+// function has access to the current retry count in its first parameter, which
+// is useful e.g when we are implementing a delay mechanism.
+func CountRetry(retryCount uint) func(RetryIndexErrorFunc) ErrorFunc {
+	return func(f RetryIndexErrorFunc) ErrorFunc {
+		var retryF RetryIndexErrorFunc
 
 		retryF = func(current uint) error {
-			if err := f(); err != nil {
+			if err := f(current); err != nil {
 				if current < retryCount {
 					return retryF(current + 1)
 				}
@@ -20,6 +30,41 @@ func Retry(f ErrorFunc, retryCount uint) ErrorFunc {
 			return nil
 		}
 
-		return retryF(0)
+		return func() error {
+			return retryF(0)
+		}
+	}
+}
+
+// Retry has the same semantics as CountRetry, but ignores the current retry
+// count.
+func Retry(retryCount uint) func(ErrorFunc) ErrorFunc {
+	return func(f ErrorFunc) ErrorFunc {
+		return CountRetry(retryCount)(func(retry uint) error {
+			return f()
+		})
+	}
+}
+
+// DelayRetry composes a function with retry-delaying capabilities. The output
+// of the return function can be fed to a CountRetry composition.
+func DelayRetry(d time.Duration) func(ErrorFunc) RetryIndexErrorFunc {
+	return func(f ErrorFunc) RetryIndexErrorFunc {
+		return func(retry uint) error {
+			if retry > 0 {
+				time.Sleep(d)
+			}
+
+			return f()
+		}
+	}
+}
+
+// RetryWithDelay composes retry with delay capabilities.
+func RetryWithDelay(retryCount uint) func(time.Duration) func(ErrorFunc) ErrorFunc {
+	return func(delay time.Duration) func(ErrorFunc) ErrorFunc {
+		return func(f ErrorFunc) ErrorFunc {
+			return CountRetry(retryCount)(DelayRetry(delay)(f))
+		}
 	}
 }
--- pkg/retry_test.go
+++ pkg/retry_test.go
@@ -3,12 +3,20 @@ package compose
 import (
 	"errors"
 	"testing"
+	"time"
+)
+
+const (
+	delayDuration = time.Duration(1e7)
+	retryCount    = uint(10)
+)
+
+var (
+	err = errors.New("error")
 )
 
 func TestRetryComposeWithAllErrors(t *testing.T) {
 	/// Setup
-	err := errors.New("error")
-	retryCount := uint(10)
 	invoked := uint(0)
 
 	errF := func() error {
@@ -17,7 +25,7 @@ func TestRetryComposeWithAllErrors(t *testing.T) {
 	}
 
 	/// When & Then
-	if err1 := Retry(errF, retryCount)(); err1 != err {
+	if err1 := Retry(retryCount)(errF)(); err1 != err {
 		t.Errorf("Expected %v, got %v", err, err1)
 	}
 
@@ -28,8 +36,6 @@ func TestRetryComposeWithAllErrors(t *testing.T) {
 
 func TestRetryComposeWithInitialError(t *testing.T) {
 	/// Setup
-	err := errors.New("error")
-	retryCount := uint(10)
 	invoked := uint(0)
 
 	errF := func() error {
@@ -45,11 +51,92 @@ func TestRetryComposeWithInitialError(t *testing.T) {
 	}
 
 	/// When & Then
-	if err1 := Retry(errF, retryCount)(); err1 != nil {
+	if err1 := Retry(retryCount)(errF)(); err1 != nil {
 		t.Errorf("Should not error, but got %v", err1)
 	}
 
 	if invoked != 2 {
 		t.Errorf("Invoked %d times", invoked)
+	}
+}
+
+func TestCountRetryCompose(t *testing.T) {
+	/// Setup
+	var currentRetry uint
+
+	errF := func(retry uint) error {
+		currentRetry = retry
+		return err
+	}
+
+	/// When & Then
+	if err1 := CountRetry(retryCount)(errF)(); err1 != err {
+		t.Errorf("Expected %v, got %v", err, err1)
+	}
+
+	if currentRetry != retryCount {
+		t.Errorf("Expected %v, got %v", retryCount, currentRetry)
+	}
+}
+
+func TestDelayRetry(t *testing.T) {
+	/// Setup
+	currentRetry := uint(2)
+
+	errF := func() error {
+		return err
+	}
+
+	/// When & Then
+	start := time.Now()
+
+	if err1 := DelayRetry(delayDuration)(errF)(currentRetry); err1 != err {
+		t.Errorf("Expected %v, got %v", err, err1)
+	}
+
+	difference := time.Now().Sub(start)
+
+	if difference < delayDuration {
+		t.Errorf("Expected %d, got %d", delayDuration, difference)
+	}
+}
+
+func TestDelayRetryForFirstInvocation(t *testing.T) {
+	/// Setup
+	errF := func() error {
+		return err
+	}
+
+	/// When & Then
+	start := time.Now()
+
+	if err1 := DelayRetry(delayDuration)(errF)(0); err1 != err {
+		t.Errorf("Expected %v, got %v", err, err1)
+	}
+
+	difference := time.Now().Sub(start)
+
+	if difference >= delayDuration {
+		t.Errorf("Should not have delayed, but got %d", difference)
+	}
+}
+
+func TestDelayedRetry(t *testing.T) {
+	/// Setup
+	errF := func() error {
+		return err
+	}
+
+	/// When & Then
+	start := time.Now()
+
+	if err1 := RetryWithDelay(retryCount)(delayDuration)(errF)(); err1 != err {
+		t.Errorf("Expected %v, got %v", err, err1)
+	}
+
+	difference := time.Now().Sub(start)
+
+	if int64(difference) < int64(delayDuration)*int64(retryCount) {
+		t.Errorf("Wrong delay duration %d", difference)
 	}
 }
